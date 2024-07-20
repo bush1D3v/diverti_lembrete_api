ffad15bca422e4bc0bb10f223cdf34131b2d7083
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-19 22:03:04 -0300

    feat: Create check reminder action

---
+++ src/Application/Actions/Reminder/CheckRemindersAction.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Reminder;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class CheckRemindersAction extends ReminderAction
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function action(Request $request, Response $response): Response
+    {
+        $reminderId = (string) $this->resolveArg('id');
+        $this->reminderRepository->check($reminderId);
+
+        $this->logger->info(sprintf("Lembrete do id %s teve o estado de 'check' atualizado.", $reminderId));
+
+        return $response->withStatus(200);
+    }
+}
