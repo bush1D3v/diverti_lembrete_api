689fc8d45b7cddbe3052d1a3193c9b7bf9c855f6
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-19 22:02:51 -0300

    update: Add check reminer route

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Application\Actions\Reminder\ViewReminderAction;
 use App\Application\Actions\Reminder\ListRemindersAction;
+use App\Application\Actions\Reminder\CheckRemindersAction;
 use App\Application\Actions\Reminder\DeleteReminderAction;
 use App\Application\Actions\Reminder\InsertReminderAction;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
@@ -16,9 +17,10 @@ return function (App $app) {
         $group->options('', function (Request $request, Response $response) {
             return $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
         });
+        $group->post('', InsertReminderAction::class);
         $group->get('', ListRemindersAction::class);
         $group->get('/{id}', ViewReminderAction::class);
-        $group->post('', InsertReminderAction::class);
+        $group->post('/{id}/check', CheckRemindersAction::class);
         $group->delete('/{id}', DeleteReminderAction::class);
     });
 };
