464768d8936fdccf5a13f872adc52ed225ff1c05
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-19 22:04:22 -0300

    refactor: Update function name

--- src/Application/Actions/Reminder/ReminderAction.php
+++ src/Application/Actions/Reminder/ReminderAction.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace App\Application\Actions\Reminder;
 
 use App\Application\Actions\Action;
-use App\Domain\Reminder\ReminderRepository;
+use App\Domain\Reminder\ReminderRepositoryInterface;
 use Psr\Log\LoggerInterface;
 
 abstract class ReminderAction extends Action
 {
-    protected ReminderRepository $reminderRepository;
+    protected ReminderRepositoryInterface $reminderRepository;
 
-    public function __construct(LoggerInterface $logger, ReminderRepository $reminderRepository)
+    public function __construct(LoggerInterface $logger, ReminderRepositoryInterface $reminderRepository)
     {
         parent::__construct($logger);
         $this->reminderRepository = $reminderRepository;
