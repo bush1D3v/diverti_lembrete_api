ac95281bea2b9cc218b10d522dd69c48fe5c1a89
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-19 22:04:39 -0300

    update: Create check interface and update list params

---
+++ src/Domain/Reminder/ReminderRepositoryInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Reminder;
+
+interface ReminderRepositoryInterface
+{
+    /**
+     * @param string $page
+     * @param string $emotion
+     * @param string $checked
+     * @return Reminder[]
+     * @throws ReminderNotFoundException
+     */
+    public function findAll(string $page = '1', string $emotion = null, string $checked = null): array;
+
+    /**
+     * @param string $id
+     * @return Reminder
+     * @throws ReminderNotFoundException
+     */
+    public function findById(string $id): Reminder;
+
+    /**
+     * @param Reminder $reminder
+     * @return void
+     */
+    public function insert(array $reminder): string;
+
+    /**
+     * @param string $id
+     * @return void
+     * @throws ReminderNotFoundException
+     */
+    public function delete(string $id): void;
+
+    /**
+     * @param string $id
+     * @return void
+     * @throws ReminderNotFoundException
+     */
+    public function check(string $id): void;
+}
