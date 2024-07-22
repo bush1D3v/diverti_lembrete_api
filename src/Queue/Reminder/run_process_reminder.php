d0309d7c5ac796bc0e136279fae36cf8b9676516
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-22 12:27:13 -0300

    refactor: Remove unused variables and update queue call

--- src/Queue/Reminder/run_process_reminder.php
+++ src/Queue/Reminder/run_process_reminder.php
@@ -2,11 +2,7 @@
 
 namespace App\Queue\Reminder;
 
-require __DIR__ . '/../../../vendor/autoload.php';
+use App\Queue\Reminder\ReminderQueues;
 
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../')->load();
-
-use App\Infrastructure\Reminder\ReminderRepository;
-
-$task = new ReminderRepository();
+$task = new ReminderQueues();
 $task->processReminder();
