35474e709bd183641a465cc1e7f948789f1c8871
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-25 22:58:12 -0300

    reset: Remove unused variables and update queue call

--- src/Queue/Reminder/run_process_reminder.php
+++ src/Queue/Reminder/run_process_reminder.php
@@ -2,6 +2,10 @@
 
 namespace App\Queue\Reminder;
 
+require __DIR__ . '/../../../vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../')->load();
+
 use App\Queue\Reminder\ReminderQueues;
 
 $task = new ReminderQueues();
