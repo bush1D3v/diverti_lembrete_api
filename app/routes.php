23b8ebef9eec416a71092aee10d5d7a3da7d07ec
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2024-07-25 22:53:58 -0300

    update: Update options & heck options and add a count route

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Application\Actions\Reminder\ViewReminderAction;
 use App\Application\Actions\Reminder\ListRemindersAction;
 use App\Application\Actions\Reminder\CheckRemindersAction;
+use App\Application\Actions\Reminder\CountRemindersAction;
 use App\Application\Actions\Reminder\DeleteReminderAction;
 use App\Application\Actions\Reminder\InsertReminderAction;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
@@ -15,18 +16,20 @@ use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 return function (App $app) {
     $app->group('/reminders', function (Group $group) {
         $group->options('', function (Request $request, Response $response) {
-            return $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+            return $response->withHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Location');
         });
         $group->post('', InsertReminderAction::class);
         $group->get('', ListRemindersAction::class);
+        $group->get('/count', CountRemindersAction::class);
         $group->options('/{id}', function (Request $request, Response $response) {
             return $response->withHeader('Access-Control-Allow-Methods', 'GET, DELETE, OPTIONS');
         });
         $group->get('/{id}', ViewReminderAction::class);
         $group->delete('/{id}', DeleteReminderAction::class);
         $group->options('/{id}/check', function (Request $request, Response $response) {
-            return $response->withHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+            return $response->withHeader('Access-Control-Allow-Methods', 'PATCH, OPTIONS');
         });
-        $group->post('/{id}/check', CheckRemindersAction::class);
+        $group->patch('/{id}/check', CheckRemindersAction::class);
     });
 };
